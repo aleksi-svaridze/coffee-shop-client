c5bbc8906d68181cea4796ec7e6e3393dad1cc7c
Natalia Kavtaradze <nataliakavtaradze777@gmail.com>
2026-01-27 21:36:09 +0400

    corrected  coffee menu dashboard

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -6,13 +6,37 @@ const coffees = [
     id: 1,
     name: "Espresso",
     price: 2.99,
-    desc: "Strong coffee",
+    desc: "Strong and concentrated coffee served in small shots.",
+  },
+  {
+    id: 3,
+    name: "Latte",
+    price: 4.29,
+    desc: "Espresso with steamed milk and a small layer of foam.",
   },
   {
     id: 2,
-    name: "Latte",
-    price: 4.29,
-    desc: "With milk",
+    name: "Cappuccino",
+    price: 3.99,
+    desc: "With milEqual parts espresso, steamed milk, and milk foam.",
+  },
+  {
+    id: 4,
+    name: "Mocha",
+    price: 4.79,
+    desc: "Espresso with chocolate, steamed milk, and whipped cream..",
+  },
+  {
+    id: 5,
+    name: "Americano",
+    price: 3.49,
+    desc: "Espresso diluted with hot water..",
+  },
+  {
+    id: 6,
+    name: "Macchiato",
+    price: 3.79,
+    desc: "Espresso with a small amount of foamed milk.",
   },
 ];
 
